e31c14b1f175a80c11e2e1e9ad1552b7e1bb6a2d
Murilo Portescheller <mport.dev@gmail.com>
2019-06-04 00:49:12 -0300

    Finished Race entity methods

--- src/entities/race.js
+++ src/entities/race.js
@@ -1,27 +1,66 @@
+const { getTimeComponents, getTimeInSeconds } = require('../modules/time');
+
 const Race = function (race) {
   this.race = race;
 
-  this.isLapBigger = function(drivers, lap) {
-    return drivers[lap.pilotId].lapNumber > drivers[lap.pilotId].lapNumber;
+  this.isLapBigger = function (pilots, lap) {
+    return pilots[lap.pilotId].lapNumber < lap.lapNumber;
   }
 
-  this.sortBy
+  this.getLapFinishTime = function (lap) {
+    const lapStartArray = getTimeComponents(lap.lapStart);
+    const lapLengthArray = getTimeComponents(lap.lapTime);
 
-  this.getResult = function () {
-    const drivers = {};
-    const { isLapBigger } = this;
+    return getTimeInSeconds(lapStartArray) + getTimeInSeconds([0, ...lapLengthArray]);
+  };
 
-    this.race.forEach(function(lap) {
-      if (drivers[lap.pilotId] && isLapBigger(drivers, lap)) {
-        drivers[lap.pilotId] = (lap.lapTimeCheck + lap.lapTime);
+  this.sortPilotsAndFormat = function (pilots) {
+    const sortedIds = Object.keys(pilots).sort((a, b) => {
+      if (pilots[a].lapNumber > pilots[b].lapNumber) {
+        return -1;
       }
 
-      if (!drivers[lap.pilotId]) {
-        drivers[lap.pilotId] = (lap.lapTimeCheck + lap.lapTime);
+      if (pilots[a].time > pilots[b].time) {
+        return -1;
+      }
+
+      return 0;
+    });
+
+    return sortedIds.map((item, index) => {
+      const startInSeconds = getTimeComponents(pilots[item].start);
+
+      return {
+        position: index + 1,
+        pilotId: item,
+        pilotName: pilots[item].pilotName,
+        laps: pilots[item].lapNumber,
+        raceTotal: Number(pilots[item].time) - getTimeInSeconds(startInSeconds),
+      };
+    });
+  }
+
+  this.getResult = function () {
+    const pilots = {};
+    const { isLapBigger, getLapFinishTime } = this;
+
+    this.race.forEach(function (lap) {
+      if (pilots[lap.pilotId] && isLapBigger(pilots, lap)) {
+        pilots[lap.pilotId].time = getLapFinishTime(lap);
+        pilots[lap.pilotId].lapNumber = lap.lapNumber;
+      }
+
+      if (!pilots[lap.pilotId]) {
+        pilots[lap.pilotId] = {
+          lapNumber: lap.lapNumber,
+          pilotName: lap.pilotName,
+          start: lap.lapStart,
+          time: getLapFinishTime(lap),
+        };
       }
     });
 
-    console.log('drivers', drivers);
+    console.log('Result', this.sortPilotsAndFormat(pilots));
   }
 }
 
