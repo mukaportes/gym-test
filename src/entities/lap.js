0dcd587b2125810411004ef2b1aa3ba3c1f96e7c
Murilo Portescheller <mport.dev@gmail.com>
2019-06-04 00:49:06 -0300

    Finished Lap entity methods

--- src/entities/lap.js
+++ src/entities/lap.js
@@ -1,23 +1,23 @@
 const { getTimeComponents, getTimeInSeconds } = require('../modules/time');
 
 const Lap = function ([
-  lapTimeCheck, pilotId, pilotName, lapNumber, lapTime, lapAverageSpeed,
+  lapStart, pilotId, pilotName, lapNumber, lapTime, lapAverageSpeed,
 ]) {
-  this.lapTimeCheck = getTimeComponents(lapTimeCheck);
+  this.lapStart = lapStart;
   this.pilotId = pilotId;
   this.pilotName = pilotName;
   this.lapNumber = lapNumber;
-  this.lapTime = getTimeComponents(lapTime);
+  this.lapTime = lapTime;
   this.lapAverageSpeed = lapAverageSpeed;
 
   this.get = function() {
     return {
-      lapTimeCheck: getTimeInSeconds(this.lapTimeCheck),
+      lapStart: this.lapStart,
       pilotId: this.pilotId,
       pilotName: this.pilotName,
       lapNumber: Number(this.lapNumber),
-      lapTime: getTimeInSeconds([0, ...this.lapTime]),
-      lapAverageSpeed: Number(this.lapAverageSpeed.replace('-', '.')),
+      lapTime: this.lapTime,
+      lapAverageSpeed: Number(this.lapAverageSpeed.replace(',', '.')),
     };
   }
 }
